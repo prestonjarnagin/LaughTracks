887169631d0c6bcbe3d79f87a890652f19d541a5
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-10-04 11:32:13 -0600

    figure out db stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
+require "./app/models/comedian.rb"
+require "activerecord-import/base"
 
+Comedian.destroy_all
+
+Comedian.create!([{
+  name: "Jim Carrey",
+  age: 56,
+  city: "Ontario"}
+  ])
+print "Created"
